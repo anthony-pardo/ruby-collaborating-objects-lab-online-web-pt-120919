d1b28ae1abd822330f335b38d5d8fcc910b26260
Anthony Pardo <pardo.anthony@gmail.com>
2020-01-20 18:38:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,7 +14,6 @@ class Song
   def self.new_by_filename(filename)
     song_details = filename.chomp(".mp3").split(" - ")
     song = Song.new(song_details[1])
-    song.artist_name = song_details[0]
   end
   
   def artist_name=(name)
