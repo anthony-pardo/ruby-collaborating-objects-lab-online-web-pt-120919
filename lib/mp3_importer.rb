d094a5c51b2284d989c0bf0997c3df301e5472c7
Anthony Pardo <pardo.anthony@gmail.com>
2020-01-20 19:03:16 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,11 +1,15 @@
 class MP3Importer 
   attr_accessor :path
-  
+  @@files = []
   def initialize(file_path)
     @path = file_path
   end
   
   def files 
     files = Dir[@path]
+    files.each do |file|
+      @@files << file
+    end
+    @@files
   end
 end
