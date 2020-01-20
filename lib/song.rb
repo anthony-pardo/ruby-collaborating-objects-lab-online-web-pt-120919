9314d457e7cd356cf9b795bd5166a927cd9e49de
Anthony Pardo <pardo.anthony@gmail.com>
2020-01-20 18:00:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,7 +12,7 @@ class Song
   end
   
   def artist_name=(name)
-    self.artist.find_or_create_by_name(name)
+    self.artist = Artist.find_or_create_by_name(name)
     self.artist.add_song(self)
   end
   
